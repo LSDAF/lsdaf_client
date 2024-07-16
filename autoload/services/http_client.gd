57ad9e481b9552a33d7086305e711aaf8d54ee52
adufou <antoine.du.fou@gmail.com>
2024-07-16 22:55:41 +0200

    chore: call generate, but fail because of async

--- autoload/services/http_client.gd
+++ autoload/services/http_client.gd
@@ -10,16 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func _http_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
-	print('result:' + str(result))
-	print('response_code:' + str(response_code))
-	print('headers:' + str(headers))
-	
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	print('body:' + str(json))
-	
-	_access_token = json["data"]["access_token"]
 
 func user_token():
 	return _access_token
@@ -33,19 +23,22 @@ static func build_login_user_request(email: String, password: String) -> String:
 	
 	return request
 
-func login_user(email: String, password: String):
-	print('login')
+func _on_login_user_success(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
+	print('result:' + str(result))
+	print('response_code:' + str(response_code))
+	print('headers:' + str(headers))
 	
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print('body:' + str(json))
+	
+	_access_token = json["data"]["access_token"]
+
+func login_user(email: String, password: String):
 	# Create an HTTP request node and connect its completion signal.
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.request_completed.connect(_http_request_completed)
+	http_request.request_completed.connect(_on_login_user_success)
 	var payload = build_login_user_request(email, password)
-	
-	# Perform a GET request. The URL below returns JSON as of writing.
-	#var error = http_request.request("https://httpbin.org/get")
-	#if error != OK:
-	#	push_error("An error occurred in the HTTP request.")
 
 	# Perform a POST request. The URL below returns JSON as of writing.
 	# Note: Don't make simultaneous requests using a single HTTPRequest node.
@@ -67,6 +60,30 @@ func me():
 	
 func get_game_save():
 	pass
+	
+func _on_generate_game_save_success(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
+	print('result:' + str(result))
+	print('response_code:' + str(response_code))
+	print('headers:' + str(headers))
+	
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print('body:' + str(json))
+
+func generate_game_save():
+	# Create an HTTP request node and connect its completion signal.
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.request_completed.connect(_on_generate_game_save_success)
+
+	# Perform a POST request. The URL below returns JSON as of writing.
+	# Note: Don't make simultaneous requests using a single HTTPRequest node.
+	# The snippet below is provided for reference only.
+		
+	var url = _app_config.backend_url + ':' + _app_config.port + '/api/v1/game_save/generate'
+		
+	var error = http_request.request(url, ['Content-Type: application/json', 'Authentication: Bearer' + _access_token], HTTPClient.METHOD_POST)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
 	
 func update_game_save():
 	pass
--- elements/main/main.gd
+++ elements/main/main.gd
@@ -2,8 +2,9 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# TODO: handle async
 	HttpClient.login_user("toto@toto.fr", "k127F978")
-
+	HttpClient.generate_game_save()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
