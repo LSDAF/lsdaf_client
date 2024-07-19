651d32534c9619ffc9fa7e8e4db16a70afbce06b
adufou <antoine.du.fou@gmail.com>
2024-07-19 19:38:44 +0200

    feat: game is in landscape, and no manual resize

--- elements/hud/hud.gd
+++ elements/hud/hud.gd
@@ -1,7 +1,5 @@
 extends Control
 
-@export var game_runtime_data: GameRuntimeData
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,10 +7,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _on_game_container_resized():
-	game_runtime_data.set_game_node_position(%GameContainer.position)
-	# TODO: No magic number ! Comes from the mirroring of the parralax layers, which come from the size of the sprite (texture)
-	var new_scale = %GameContainer.size.x / 576
-	game_runtime_data.set_game_node_scale(Vector2(new_scale, new_scale))
-	pass # Replace with function body.
