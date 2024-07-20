7f1dec6ef0f19be84d46dfe8de62a60fa25f121f
adufou <antoine.du.fou@gmail.com>
2024-07-20 21:54:20 +0200

    feat: cost formula in resource for attack and hp

--- models/characteristic/characteristic.gd
+++ models/characteristic/characteristic.gd
@@ -5,10 +5,15 @@ signal upgraded
 
 @export var name: String
 
+# From this idea https://stackoverflow.com/questions/76700683/what-formulas-to-use-for-clicker-incremental-games
+@export var cost_exponent: float
+@export var cost_coef: float
+@export var cost_base: int
+
 var _level: int = 1
 
 func next_level_cost() -> int:
-	return 1
+	return cost_coef * (_level ** cost_exponent) + cost_base
 
 func current_value() -> int:
 	return _level * 2
