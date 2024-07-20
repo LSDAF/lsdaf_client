b99ad196be921d2dc71867f64d2297380f07e308
adufou <antoine.du.fou@gmail.com>
2024-07-20 12:29:42 +0200

    chore: force types, game landscape and scales

--- elements/hud/currencies/gold_label.gd
+++ elements/hud/currencies/gold_label.gd
@@ -2,13 +2,13 @@ extends Label
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	Currencies.gold.updated.connect(_update_gold_value)
 	_update_gold_value(Currencies.gold.get_value())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	pass
 
-func _update_gold_value(new_value: int):
+func _update_gold_value(new_value: int) -> void:
 	text = str(new_value)
