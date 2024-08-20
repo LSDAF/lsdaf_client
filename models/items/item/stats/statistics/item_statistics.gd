d9a5880987add8dbf256b5a81e0a251d1e8085f8
adufou <antoine.du.fou@gmail.com>
2024-08-20 22:59:37 +0200

    feat: added more blueprints, but need to clean the files

--- models/items/item/stats/statistics/item_statistics.gd
+++ models/items/item/stats/statistics/item_statistics.gd
@@ -3,6 +3,10 @@ class_name ItemStatistics
 enum ItemStatistics {
     ATTACK_ADD,
     ATTACK_MULT,
+    CRIT_CHANCE,
+    CRIT_DAMAGE,
     HP_ADD,
     HP_MULT,
+    RESISTANCE_ADD,
+    RESISTANCE_MULT,
 }
