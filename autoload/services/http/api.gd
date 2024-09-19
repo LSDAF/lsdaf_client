94bc6e9d578364583cbdac09c36b44e010945efb
adufou <antoine.du.fou@gmail.com>
2024-09-19 17:37:26 +0200

    feat: add fetch games

--- autoload/services/http/api.gd
+++ autoload/services/http/api.gd
@@ -1,3 +1,6 @@
 extends Node
 
+var access_token: String = ''
+
 var auth: Auth = preload("res://http/auth/auth.gd").new()
+var user: User = preload("res://http/user/user.gd").new()
--- http/user/user.gd
+++ http/user/user.gd
@@ -1,29 +1,23 @@
-class_name Auth
+class_name User
 
-func login(email: String, password: String, on_failure: Callable) -> LoginResponseDto:
-	var body := JSON.stringify({
-		"email": email,
-		"password": password,
-	})
-
+func fetch_game_saves(on_failure: Callable) -> void:
 	var response: HTTPResult = await Http.http.async_request(
-		ApiRoutes.LOGIN,
-		['Content-Type: application/json'],
-		HTTPClient.METHOD_POST,
-		body
+		ApiRoutes.FETCH_GAME_SAVES,
+		['Authorization: Bearer {0}'.format([Api.access_token])],
+		HTTPClient.METHOD_GET,
 	)
 
 	if !response.success() or response.status_err():
 		push_error("Request failed.")
-		return null
+		return
 
 	print("Status code: ", response.status)
 	print("Content-Type:", response.headers["content-type"])
 
 	var json: Dictionary = response.body_as_json()
 
+	print(json)
+
 	if not json or not json['data']:
 		push_error("JSON invalid.")
-		return null
-
-	return LoginResponseDto.new(json['data'])
+		return
