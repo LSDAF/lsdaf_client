74f6b0f008d94efbfcc60b24591c7aef4d943bef
adufou <antoine.du.fou@gmail.com>
2024-09-23 23:10:41 +0200

    feat: mvp of game save loading after login in

---
+++ autoload/services/game_save/game_save_service.gd
@@ -0,0 +1,6 @@
+extends Node
+
+func load_game_save(game_save_dto: GameSaveDto) -> void:
+	Currencies.gold.update_value(game_save_dto.gold)
+
+	print(Currencies.gold)
--- elements/launcher/game_save.gd
+++ elements/launcher/game_save.gd
@@ -1,22 +1,23 @@
 extends Control
 
-class_name GameSaveNode
+class_name GameSave
 
-var _game_id: String
-var _gold: int
-var _created_at: int
+signal game_loaded
+
+var _game_save_dto: GameSaveDto
 
 
 func _ready() -> void:
-	%GameIdLabel.text = _game_id
-	%GameGoldLabel.text = 'Gold: {0}'.format([str(_gold)])
-	%GameCreatedAtLabel.text = 'Created: {0}'.format([Time.get_datetime_string_from_unix_time(_created_at)])
+	%GameIdLabel.text = _game_save_dto.id
+	%GameGoldLabel.text = 'Gold: {0}'.format([str(_game_save_dto.gold)])
+	%GameCreatedAtLabel.text = 'Created: {0}'.format([Time.get_datetime_string_from_unix_time(_game_save_dto.created_at)])
 
-func initialize(game_id: String, gold: int, created_at: int) -> void:
-	_game_id = game_id
-	_gold = gold
-	_created_at = created_at
+func initialize(game_save_dto: GameSaveDto, on_game_loaded: Callable) -> void:
+	_game_save_dto = game_save_dto
+	game_loaded.connect(on_game_loaded)
 
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	print("clicked load game")
+	GameSaveService.load_game_save(_game_save_dto)
+	game_loaded.emit()
