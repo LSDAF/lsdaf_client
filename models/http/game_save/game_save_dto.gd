e44e376986a49475567b94ed156b3682edba14d7
adufou <antoine.du.fou@gmail.com>
2024-09-30 21:49:41 +0200

    feat: see the nickname of the game save

--- models/http/game_save/game_save_dto.gd
+++ models/http/game_save/game_save_dto.gd
@@ -3,10 +3,11 @@ class_name GameSaveDto
 var id: String
 var created_at: int
 var gold: int
+var nickname: String
 
 
 func _init(dictionary: Dictionary) -> void:
 	id = dictionary["id"]
-
 	created_at = Time.get_unix_time_from_datetime_string(dictionary["created_at"])
 	gold = dictionary["gold"]
+	nickname = dictionary["nickname"]
