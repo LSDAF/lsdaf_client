00d9ee516d51eeb8b9054a1c21cb06915dea1d04
adufou <antoine.du.fou@gmail.com>
2024-10-01 00:00:38 +0200

    refacto: refactor http_client autoload

--- autoload/http/http.gd
+++ autoload/http/http.gd
@@ -1,3 +1,11 @@
 extends Node
 
-@export var http: AwaitableHTTPRequest
+# Scripts
+
+# Scenes (CAUTION: The scenes need to be instanced in the _ready function)
+var http_client: HttpClient = preload("res://autoload/http/http_client/http_client.tscn").instantiate()
+
+
+
+func _ready() -> void:
+	add_child(http_client)
