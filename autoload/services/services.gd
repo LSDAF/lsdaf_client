391cdec400d1afca2958eb0a55c73b2263c8396f
adufou <antoine.du.fou@gmail.com>
2024-09-30 23:35:28 +0200

    refacto: refactor game_save_service autoload

--- autoload/services/services.gd
+++ autoload/services/services.gd
@@ -1,1 +1,10 @@
 extends Node
+
+# Scripts
+var game_save_service: GameSaveService = preload("res://autoload/services/game_save/game_save_service.gd").new()
+
+# Scenes (CAUTION: The scenes need to be instanced in the _ready function)
+
+
+func _ready() -> void:
+	pass
