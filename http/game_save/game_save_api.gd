f13beb4c3b111861a1c673d4b7081e15131d1c0c
adufou <antoine.du.fou@gmail.com>
2024-10-22 20:02:20 +0200

    refacto: move currency api to its own file

--- http/game_save/game_save_api.gd
+++ http/game_save/game_save_api.gd
@@ -36,39 +36,3 @@ func update_game_save_nickname(
 		return false
 
 	return true
-
-
-func update_game_save_currencies(
-	game_save_id: String,
-	gold: int,
-	diamonds: int,
-	emeralds: int,
-	amethysts: int,
-	on_failure: Callable
-) -> bool:
-	var body: Dictionary = {}
-
-	if gold != null:
-		body["gold"] = gold
-
-	if diamonds != null:
-		body["diamonds"] = diamonds
-
-	if emeralds != null:
-		body["emeralds"] = emeralds
-
-	if amethysts != null:
-		body["amethysts"] = amethysts
-
-	var response: HTTPResult = await Http.api_client.post(
-		Http.api_routes.UPDATE_GAME_SAVE_CURRENCIES.format({"game_save_id": game_save_id}),
-		true,
-		body
-	)
-
-	if !response.success() or response.status_err():
-		push_error("Request failed.")
-		on_failure.call(response)
-		return false
-
-	return true
