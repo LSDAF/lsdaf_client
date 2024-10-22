8c2705ef07df6cc2a7d082f6a85b5d490dc30114
adufou <antoine.du.fou@gmail.com>
2024-10-22 22:24:23 +0200

    refacto: migrate to game save data

--- autoload/data/data.gd
+++ autoload/data/data.gd
@@ -3,6 +3,7 @@ extends Node
 # Scripts
 var currencies: Currencies = preload("res://autoload/data/currencies/currencies.gd").new()
 var difficulty: Difficulty = preload("res://autoload/data/difficulty/difficulty.gd").new()
+var game_save: GameSave = preload("res://autoload/data/game_save/game_save.gd").new()
 var inventory: Inventory = preload("res://autoload/data/inventory/inventory.gd").new()
 var stage: Stage = preload("res://autoload/data/stage/stage.gd").new()
 
