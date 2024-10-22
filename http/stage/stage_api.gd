063fc17fc7a4370e9187dec3e9fb083dabf85aa1
adufou <antoine.du.fou@gmail.com>
2024-10-22 22:38:02 +0200

    chore: apply gdformat on http folder

--- http/stage/stage_api.gd
+++ http/stage/stage_api.gd
@@ -1,5 +1,6 @@
 class_name StageApi
 
+
 func fetch_game_save_stage(game_save_id: String, on_failure: Callable) -> FetchStageDto:
 	var response: HTTPResult = await Http.api_client.fetch(
 		Http.api_routes.FETCH_GAME_SAVES_STAGE.format({"game_save_id": game_save_id}), true
@@ -20,7 +21,9 @@ func fetch_game_save_stage(game_save_id: String, on_failure: Callable) -> FetchS
 	return FetchStageDto.new(json["data"])
 
 
-func update_game_save_stage(game_save_id: String, update_stage_dto: UpdateStageDto, on_failure: Callable) -> bool:
+func update_game_save_stage(
+	game_save_id: String, update_stage_dto: UpdateStageDto, on_failure: Callable
+) -> bool:
 	var response: HTTPResult = await Http.api_client.post(
 		Http.api_routes.UPDATE_GAME_SAVE_STAGE.format({"game_save_id": game_save_id}),
 		true,
