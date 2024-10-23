a6fc24a9ce5220f439b73fe1baf64bb175fb72ba
adufou <antoine.du.fou@gmail.com>
2024-10-23 22:58:39 +0200

    feat: add difficulty service tests

---
+++ src/autoload/services/difficulty/__tests__/difficulty_service_test.gd
@@ -0,0 +1,30 @@
+# GdUnit generated TestSuite
+class_name DifficultyServiceTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://src/autoload/services/difficulty/difficulty_service.gd'
+
+
+func test_set_current_difficulty() -> void:
+	# Arrange
+	Data.difficulty._current_difficulty = 420.69
+
+	# Act
+	Services.difficulty.set_current_difficulty(1.0)
+
+	# Assert
+	assert_float(Services.difficulty.get_current_difficulty()).is_equal(1.0)
+
+
+func test_get_current_difficulty() -> void:
+	# Arrange
+	Data.difficulty._current_difficulty = 420.69
+
+	# Act
+	var current_difficulty: float = Services.difficulty.get_current_difficulty()
+
+	# Assert
+	assert_float(current_difficulty).is_equal(420.69)
