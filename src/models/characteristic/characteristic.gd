3a5c1ef60ee4dfd145df573301189c804cb6c73d
adufou <antoine.du.fou@gmail.com>
2024-10-27 15:49:04 +0100

    chore: add gdlint to pre commit

--- src/models/characteristic/characteristic.gd
+++ src/models/characteristic/characteristic.gd
@@ -5,7 +5,6 @@ signal upgraded
 
 @export var name: String
 
-# From this idea https://stackoverflow.com/questions/76700683/what-formulas-to-use-for-clicker-incremental-games
 @export var cost_scaler: CostScaler
 
 var _level: int = 1
