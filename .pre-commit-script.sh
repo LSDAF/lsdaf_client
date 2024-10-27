a5f7289f36e40f4be4fc8b5eb66dd5576ebc8bef
adufou <antoine.du.fou@gmail.com>
2024-10-27 15:49:04 +0100

    chore: add gdlint to pre commit

--- .pre-commit-script.sh
+++ .pre-commit-script.sh
@@ -7,25 +7,36 @@ python3 -m venv .venv
 source .venv/bin/activate
 
 # Install the required packages
-echo "Installing requirements..."
 pip install --quiet -r requirements.txt
 
 # Get a list of staged files
 STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACM)
 
-# Run gdformat on each staged file
+# Run gdformat and gdlint on each staged file
+gdlint_errors=()
+
 for file in $STAGED_FILES; do
-    if [[ $file == *.gd || $file == *.gdscript ]]; then
+    if [[ $file == *.gd ]]; then
         echo "Running gdformat on $file..."
         gdformat "$file"
+        echo "Running gdlint on $file..."
+        if gdlint "$file"; then
+          errored_files+=("$file")
+        fi
     fi
 done
 
+if [ ${#gdlint_errors[@]} -ne 0 ]; then
+  for error in "${gdlint_errors[@]}"; do
+      echo " - Gdlint error: $error"
+  done
+
+  echo "Gdlint errors: [ ${#gdlint_errors[@]} ] found, aborting commit"
+  exit 1
+fi
 # Optionally, you can add additional commands or checks here
 
-# Staged files
+# Add staged files to git
 for file in $STAGED_FILES; do
-    if [[ $file == *.gd || $file == *.gdscript ]]; then
-        git add "$file"
-    fi
+    git add "$file"
 done
