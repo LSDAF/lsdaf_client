a2be14e762dd85db2a7e1088ef5fd7494d640355
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-02 22:16:31 +0100

    lint: linted code

--- src/models/http/request/http_request.gd
+++ src/models/http/request/http_request.gd
@@ -4,10 +4,12 @@ var _function: Callable
 var _nb_retries: int
 var _prioritary: bool
 
+
 func _init(dictionary: Dictionary) -> void:
 	_function = dictionary["function"]
 	_nb_retries = dictionary["nb_retries"]
 	_prioritary = dictionary["prioritary"]
+
 
 func to_dictionary() -> Dictionary:
 	return {
@@ -16,11 +18,14 @@ func to_dictionary() -> Dictionary:
 		"prioritary": _prioritary,
 	}
 
+
 func get_function() -> Callable:
 	return _function
 
+
 func get_nb_retries() -> int:
 	return _nb_retries
+
 
 func get_prioritary() -> bool:
 	return _prioritary
