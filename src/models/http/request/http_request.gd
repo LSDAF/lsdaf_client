99928f6ab88dc90129ae861bd125e4cdb8b742df
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-02 22:15:04 +0100

    chore: added http_request for dto for HTTP event handler

---
+++ src/models/http/request/http_request.gd
@@ -0,0 +1,26 @@
+class_name HttpRequest
+
+var _function: Callable
+var _nb_retries: int
+var _prioritary: bool
+
+func _init(dictionary: Dictionary) -> void:
+	_function = dictionary["function"]
+	_nb_retries = dictionary["nb_retries"]
+	_prioritary = dictionary["prioritary"]
+
+func to_dictionary() -> Dictionary:
+	return {
+		"function": _function,
+		"nb_retries": _nb_retries,
+		"prioritary": _prioritary,
+	}
+
+func get_function() -> Callable:
+	return _function
+
+func get_nb_retries() -> int:
+	return _nb_retries
+
+func get_prioritary() -> bool:
+	return _prioritary
