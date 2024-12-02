493f3f61175578c1651b48a80ba325b06f003475
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-12-03 00:35:52 +0100

    lint: reformatted code + linted code

--- src/elements/game_main/game_main.gd
+++ src/elements/game_main/game_main.gd
@@ -3,6 +3,8 @@ extends Node
 
 signal on_logout
 
+var _lambda := func() -> void: Services.game_save.save_game()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,13 +15,10 @@ func _on_logout() -> void:
 	on_logout.emit()
 
 
-var lambda := func() -> void: Services.game_save.save_game()
-
-
 func _on_game_save_timer_timeout() -> void:
 	#	Services.game_save.save_game()
 	var http_request: HttpEvent = HttpEvent.new(
-		{"function": lambda, "nb_tries": 1, "prioritary": true}
+		{"function": _lambda, "nb_tries": 1, "prioritary": true}
 	)
 	Services.http_event_handler.enqueue_event(http_request)
 	Services.http_event_handler.process_queue(false)
