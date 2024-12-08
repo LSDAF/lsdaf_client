2f0830e4f978452796d1f08a8384d32a372ea69d
adufou <antoine.du.fou@gmail.com>
2024-12-08 19:55:54 +0100

    feat: move prettify to stat and rarity enum class

--- src/models/items/item/stats/statistics/item_statistics.gd
+++ src/models/items/item/stats/statistics/item_statistics.gd
@@ -10,3 +10,31 @@ enum ItemStatistics {
 	RESISTANCE_ADD,
 	RESISTANCE_MULT,
 }
+
+
+static func _prettify_statistic(
+	item_statistic: ItemStatistics.ItemStatistics, current_value: float
+) -> String:
+	var prettified_statistic := ""
+
+	match item_statistic:
+		ItemStatistics.ItemStatistics.ATTACK_ADD:
+			prettified_statistic = "+{0} {1}".format([current_value, "Attack"])
+		ItemStatistics.ItemStatistics.ATTACK_MULT:
+			prettified_statistic = "+{0}% {1}".format([current_value, "Attack"])
+		ItemStatistics.ItemStatistics.CRIT_CHANCE:
+			prettified_statistic = "+{0}% {1}".format([current_value, "Crit. Chance"])
+		ItemStatistics.ItemStatistics.CRIT_DAMAGE:
+			prettified_statistic = "+{0}% {1}".format([current_value, "Crit. Damage"])
+		ItemStatistics.ItemStatistics.HEALTH_ADD:
+			prettified_statistic = "+{0} {1}".format([current_value, "Health"])
+		ItemStatistics.ItemStatistics.HEALTH_MULT:
+			prettified_statistic = "+{0}% {1}".format([current_value, "Health"])
+		ItemStatistics.ItemStatistics.RESISTANCE_ADD:
+			prettified_statistic = "+{0} {1}".format([current_value, "Resistance"])
+		ItemStatistics.ItemStatistics.RESISTANCE_MULT:
+			prettified_statistic = "+{0}% {1}".format([current_value, "Resistance"])
+		_:
+			prettified_statistic = "UNSUPPORTED"
+
+	return prettified_statistic
