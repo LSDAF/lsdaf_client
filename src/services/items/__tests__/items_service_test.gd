fc85f3d46191bafa1508d4e3749003b9d6840b84
adufou <antoine.du.fou@gmail.com>
2024-12-08 20:26:24 +0100

    feat: use uuid to create the client id of item

--- src/services/items/__tests__/items_service_test.gd
+++ src/services/items/__tests__/items_service_test.gd
@@ -4,6 +4,10 @@ extends GutTest
 
 var sut: ItemsService
 
+var game_save_service := preload("res://src/services/game_save/game_save_service.gd")
+
+var game_save_service_partial_double: Variant
+
 var AttackAdd := preload("res://src/resources/items/stats/attack_add.tres")
 var AttackMult := preload("res://src/resources/items/stats/attack_mult.tres")
 var CritChance := preload("res://src/resources/items/stats/crit_chance.tres")
@@ -15,7 +19,9 @@ var ResistanceMult := preload("res://src/resources/items/stats/resistance_mult.t
 
 
 func before_each() -> void:
-	sut = preload("res://src/services/items/items_service.gd").new()
+	game_save_service_partial_double = partial_double(game_save_service).new()
+
+	sut = preload("res://src/services/items/items_service.gd").new(game_save_service_partial_double)
 
 
 func test_create_item() -> void:
@@ -29,6 +35,7 @@ func test_create_item() -> void:
 	# Assert
 	assert_eq(item.type, item_type)
 	assert_eq(item.rarity, item_rarity)
+	assert_not_null(item.client_id)
 
 
 func test_get_additional_stats() -> void:
