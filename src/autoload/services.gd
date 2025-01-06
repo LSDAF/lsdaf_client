547bd5b1ae3dbfed1125e8a4d560dee7cb867970
Louis SANTUCCI <louissantucci1998@gmail.com>
2025-01-06 18:35:15 +0100

    feat: added timer to http event handler for retry mechanism

--- src/autoload/services.gd
+++ src/autoload/services.gd
@@ -59,9 +59,10 @@ var game_save: GameSaveService = preload("res://src/services/game_save/game_save
 	Data.game_save
 )
 
-var http_event_handler: HttpEventHandler = (
-	preload("res://src/services/http_event_handler/http_event_handler.gd")
-	. new(Queue.new(), Queue.new())
+var http_event_handler: HttpEventHandler = preload("res://src/services/http_event_handler/http_event_handler.gd").new(
+		Queue.new(),
+		Queue.new(),
+		timer
 )
 
 
