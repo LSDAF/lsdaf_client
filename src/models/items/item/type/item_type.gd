dbbd455132efadef21d645c1e1ab64c4d324b478
adufou <antoine.du.fou@gmail.com>
2025-01-23 22:05:24 +0100

    feat: display item type

--- src/models/items/item/type/item_type.gd
+++ src/models/items/item/type/item_type.gd
@@ -8,3 +8,21 @@ enum ItemType {
 	SHIELD,
 	SWORD,
 }
+
+
+static func _prettify_type_short(item_type: ItemType.ItemType) -> String:
+	match item_type:
+		ItemType.ItemType.BOOTS:
+			return "Bo."
+		ItemType.ItemType.CHESTPLATE:
+			return "Ch."
+		ItemType.ItemType.GLOVES:
+			return "Gl."
+		ItemType.ItemType.HELMET:
+			return "He."
+		ItemType.ItemType.SHIELD:
+			return "Sh."
+		ItemType.ItemType.SWORD:
+			return "Sw."
+
+	return "Unknown"
