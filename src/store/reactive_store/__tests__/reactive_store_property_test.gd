a5d6a4e64654e5f09eb9b90287f65ba92d262a43
adufou <antoine.du.fou@gmail.com>
2025-02-09 16:00:01 +0100

    feat: add ReactiveStoreProperty tests
    - Add new test file for ReactiveStoreProperty with tests for get_value and set_value functionality
    - Update .windsurfrules with additional commit-related guidelines

---
+++ src/store/reactive_store/__tests__/reactive_store_property_test.gd
@@ -0,0 +1,39 @@
+extends GutTest
+
+var sut: ReactiveStoreProperty
+var store: ReactiveStore
+
+
+func before_each() -> void:
+	store = ReactiveStore.new()
+	store._define_properties({&"test_property": TYPE_STRING}, {&"test_property": ""})
+	sut = ReactiveStoreProperty.new(store, &"test_property")
+	add_child(store)
+	add_child(sut)
+
+
+func after_each() -> void:
+	sut.queue_free()
+	store.queue_free()
+
+
+func test_get_value_returns_store_property_value() -> void:
+	# Arrange
+	store.set_property(&"test_property", "test_value")
+
+	# Act
+	var value = await sut.get_value()
+
+	# Assert
+	assert_eq(value, "test_value")
+
+
+func test_set_value_updates_store_property() -> void:
+	# Arrange
+
+	# Act
+	sut.set_value("new_value")
+	var value = await store._get_property(&"test_property")
+
+	# Assert
+	assert_eq(value, "new_value")
