8c5abff3170b51f711fb81b1daf013f38efda114
adufou <antoine.du.fou@gmail.com>
2025-02-14 23:37:09 +0100

    chore: update gut to 9.3.1

--- addons/gut/autofree.gd
+++ addons/gut/autofree.gd
@@ -5,7 +5,7 @@
 # The MIT License (MIT)
 # =====================
 #
-# Copyright (c) 2020 Tom "Butch" Wesley
+# Copyright (c) 2025 Tom "Butch" Wesley
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -31,32 +31,27 @@
 var _to_free = []
 var _to_queue_free = []
 
-
 func add_free(thing):
-	if typeof(thing) == TYPE_OBJECT:
-		if !thing is RefCounted:
+	if(typeof(thing) == TYPE_OBJECT):
+		if(!thing is RefCounted):
 			_to_free.append(thing)
-
 
 func add_queue_free(thing):
 	_to_queue_free.append(thing)
 
-
 func get_queue_free_count():
 	return _to_queue_free.size()
-
 
 func get_free_count():
 	return _to_free.size()
 
-
 func free_all():
 	for i in range(_to_free.size()):
-		if is_instance_valid(_to_free[i]):
+		if(is_instance_valid(_to_free[i])):
 			_to_free[i].free()
 	_to_free.clear()
 
 	for i in range(_to_queue_free.size()):
-		if is_instance_valid(_to_queue_free[i]):
+		if(is_instance_valid(_to_queue_free[i])):
 			_to_queue_free[i].queue_free()
 	_to_queue_free.clear()
--- addons/gut/gut_plugin.gd
+++ addons/gut/gut_plugin.gd
@@ -3,7 +3,6 @@ extends EditorPlugin
 
 var _bottom_panel = null
 
-
 func _version_conversion():
 	var EditorGlobals = load("res://addons/gut/gui/editor_globals.gd")
 	EditorGlobals.create_temp_directory()
@@ -11,13 +10,12 @@ func _version_conversion():
 	var VersionConversion = load("res://addons/gut/version_conversion.gd")
 	VersionConversion.convert()
 
-
 func _enter_tree():
 	_version_conversion()
 
-	_bottom_panel = preload("res://addons/gut/gui/GutBottomPanel.tscn").instantiate()
+	_bottom_panel = preload('res://addons/gut/gui/GutBottomPanel.tscn').instantiate()
 
-	var button = add_control_to_bottom_panel(_bottom_panel, "GUT")
+	var button = add_control_to_bottom_panel(_bottom_panel, 'GUT')
 	button.shortcut_in_tooltip = true
 
 	# ---------
@@ -42,6 +40,7 @@ func _exit_tree():
 	# Always remember to remove_at it from the engine when deactivated
 	remove_control_from_bottom_panel(_bottom_panel)
 	_bottom_panel.free()
+
 
 # This seems like a good idea at first, but it deletes the settings for ALL
 # projects.  If by chance you want to do that you can uncomment this, reload the
