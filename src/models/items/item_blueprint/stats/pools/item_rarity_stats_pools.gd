db8f1128ce8696e1fe3c7276d3b752b228c80ced
adufou <antoine.du.fou@gmail.com>
2025-02-20 22:51:24 +0100

    feat: add epic, legendary, and mythic items
    Added epic, legendary, and mythic rarities for all item types:
    
    - Created blueprint files for each rarity tier:
      - Epic items (2 variants each)
      - Legendary items (2 variants each)
      - Mythic items (2 variants each)
    
    - Updated item pools to include new rarities:
      - boots.tres
      - chestplates.tres
      - gloves.tres
      - helmets.tres
      - shields.tres
      - swords.tres
    
    - All tests passing

--- src/models/items/item_blueprint/stats/pools/item_rarity_stats_pools.gd
+++ src/models/items/item_blueprint/stats/pools/item_rarity_stats_pools.gd
@@ -4,3 +4,6 @@ extends Resource
 @export var normal: ItemStatsPool
 @export var rare: ItemStatsPool
 @export var magic: ItemStatsPool
+@export var epic: ItemStatsPool
+@export var legendary: ItemStatsPool
+@export var mythic: ItemStatsPool
