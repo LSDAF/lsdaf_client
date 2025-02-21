06ac173640575344934845368681d0e64f504f86
adufou <antoine.du.fou@gmail.com>
2025-02-21 11:23:02 +0100

    fix: update texture paths to use correct plural forms for boots and gloves
    - Modified update_textures.py to keep plural forms for boots and gloves
    - Updated all item resource files with correct texture paths
    - Fixed missing texture errors for boots and gloves items

--- scripts/update_textures.py
+++ scripts/update_textures.py
@@ -79,51 +79,58 @@ def process_directory(base_dir):
             else:
                 item_type_in_path = item_type
             
+            # Remove plural from item type (e.g., 'chestplates' -> 'chestplate')
+            # Keep plural for boots and gloves, singular for others
+            if item_type_in_path.lower() in ['boots', 'gloves']:
+                type_name_final = item_type_in_path
+            else:
+                type_name_final = item_type_in_path.rstrip('s')
+            
             if 'normal_' in file:
                 if '_1.' in file:
                     number = "1" if is_shield else "2"
-                    texture_path = f"res://asset/items/items/{base_path}/Copper/Copper_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Copper/Copper_{type_name_final.capitalize()}{number}.png"
                     print(f"Generated texture path: {texture_path}")
                 elif '_2.' in file:
                     number = "3" if is_shield else "7"
-                    texture_path = f"res://asset/items/items/{base_path}/Copper/Copper_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Copper/Copper_{type_name_final.capitalize()}{number}.png"
             elif 'rare_' in file:
                 if '_1.' in file:
                     number = "2" if is_shield else "5"
-                    texture_path = f"res://asset/items/items/{base_path}/Iron/Iron_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Iron/Iron_{type_name_final.capitalize()}{number}.png"
                 elif '_2.' in file:
                     number = "4" if is_shield else "6"
-                    texture_path = f"res://asset/items/items/{base_path}/Iron/Iron_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Iron/Iron_{type_name_final.capitalize()}{number}.png"
             elif 'magic_' in file:
                 if '_1.' in file:
                     number = "1" if is_shield else "5"
-                    texture_path = f"res://asset/items/items/{base_path}/Cobalt/Cobalt_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Cobalt/Cobalt_{type_name_final.capitalize()}{number}.png"
                 elif '_2.' in file:
                     number = "3" if is_shield else "9"
-                    texture_path = f"res://asset/items/items/{base_path}/Cobalt/Cobalt_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Cobalt/Cobalt_{type_name_final.capitalize()}{number}.png"
             elif 'epic_' in file:
                 if '_1.' in file:
                     number = "2" if is_shield else "14"
                     material = "Silver" if is_weapon else "Nova"
-                    texture_path = f"res://asset/items/items/{base_path}/{material}/{material}_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/{material}/{material}_{type_name_final.capitalize()}{number}.png"
                 elif '_2.' in file:
                     number = "4" if is_shield else "16"
                     material = "Silver" if is_weapon else "Nova"
-                    texture_path = f"res://asset/items/items/{base_path}/{material}/{material}_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/{material}/{material}_{type_name_final.capitalize()}{number}.png"
             elif 'legendary_' in file:
                 if '_1.' in file:
                     number = "1" if is_shield else "11"
-                    texture_path = f"res://asset/items/items/{base_path}/Platinum/Platinum_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Platinum/Platinum_{type_name_final.capitalize()}{number}.png"
                 elif '_2.' in file:
                     number = "3" if is_shield else "17"
-                    texture_path = f"res://asset/items/items/{base_path}/Platinum/Platinum_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Platinum/Platinum_{type_name_final.capitalize()}{number}.png"
             elif 'mythic_' in file:
                 if '_1.' in file:
                     number = "2" if is_shield else "16"
-                    texture_path = f"res://asset/items/items/{base_path}/Angelic/Angelic_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Angelic/Angelic_{type_name_final.capitalize()}{number}.png"
                 elif '_2.' in file:
                     number = "4" if is_shield else "17"
-                    texture_path = f"res://asset/items/items/{base_path}/Angelic/Angelic_{item_type_in_path.capitalize()}{number}.png"
+                    texture_path = f"res://asset/items/items/{base_path}/Angelic/Angelic_{type_name_final.capitalize()}{number}.png"
             else:
                 continue
                 
