372dec29fe1b78c7265790fe389d79d6c919b7b5
adufou <antoine.du.fou@gmail.com>
2025-02-22 13:35:11 +0100

    chore: fill rarity specs

--- src/models/items/item/rarity_specs/rarity_spec.gd
+++ src/models/items/item/rarity_specs/rarity_spec.gd
@@ -3,5 +3,6 @@ extends Resource
 
 @export var range_min_mulitplier: float
 @export var range_max_mulitplier: float
-@export var prefix_number: int
-@export var suffix_number: int
+@export var min_prefix_number: int
+@export var min_suffix_number: int
+@export var total_affix_number: int
